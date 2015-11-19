4bbb59ed6f20bb540d7e5034dd167f3320021b12
punyapats <evilpun1402@gmail.com>
2015-11-19 23:50:28 +0700

    new schema /wo validate and not attach to model yet

---
+++ packages/opd-model/schemas/disease-schema.js
@@ -0,0 +1,20 @@
+OPD.Schema.Disease = new SimpleSchema({
+    ICD: {
+      type: String,
+      label: 'รหัสโรค',
+      max: 15
+    },
+    Name: {
+      type: String,
+      label: 'ชื่อโรค',
+      max: 50
+    },
+    Description: {
+      type: String,
+      label: 'คำอธิบาย',
+      max: 200
+    },
+    
+});
+
+//Model.Patients.attachSchema(Schema.Patients);
---
+++ packages/opd-model/schemas/medicine-schema.js
@@ -0,0 +1,19 @@
+OPD.Schema.Medicine = new SimpleSchema({
+    ID: {
+      type: String,
+      label: 'รหัสยา',
+      max: 15
+    },
+    Name: {
+      type: String,
+      label: 'ชื่อยา',
+      max: 50
+    },
+    Description: {
+      type: String,
+      label: 'คำอธิบาย',
+      max: 200
+    }, 
+});
+
+//Model.Patients.attachSchema(Schema.Patients);
