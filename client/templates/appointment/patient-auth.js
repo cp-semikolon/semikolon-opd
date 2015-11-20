c388c51e5b7cff44d2f6948d9f6c0343d23c4a19
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-11-20 09:52:18 +0700

    add reactive dict and restructure dispatcher registration

--- client/templates/appointment/patient-auth.js
+++ client/templates/appointment/patient-auth.js
@@ -2,21 +2,42 @@
 let newAppointmentPath = '/appointment/new';
 
 class PatientAuth extends BlazeComponent {
+  onCreated() {
+    super.onCreated();
 
+    this.state = new ReactiveDict();
+    this.state.set('require_otp', false);
+    this.state.set('patientAuthFormType', 'method');
+
+    registerDispatcher(this.state);
+  }
+
+  isRequireOTP() {
+    return this.state.get('require_otp');
+  }
+
+  patientAuthFormState() {
+    return this.state.get('patientAuthFormType');
+  }
 }
 
-// Action dispatcher for the template
+// Action dispatcher for this component
 
-Dispatcher.register(action => {
-  switch( action.type ) {
-    case "PATIENT_MAKE_APPOINTMENT_REQUEST":
-      FlowRouter.go(newAppointmentPath);
-      break;
+function registerDispatcher(state) {
 
-    case "PATIENT_AUTHENTICATION_FAIL":
-      console.log('fail');
-      break;
-  }
-});
+  Dispatcher.register(action => {
+      switch( action.type ) {
+        case "PATIENT_MAKE_APPOINTMENT_REQUEST":
+          FlowRouter.go(newAppointmentPath);
+          break;
+
+        case "PATIENT_REQUIRE_OTP":
+          state.set('require_otp', true);
+          this.state.set('patientAuthFormType', 'disabled');
+          break;
+      }
+    });
+
+}
 
 PatientAuth.register('PatientAuth');
