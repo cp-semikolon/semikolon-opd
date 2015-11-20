e62527940563eab4e84a749c52cb6ae301f2e934
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-11-20 16:13:37 +0700

    resolve crash by not providing schema from opd-model

--- packages/opd-model/package.js
+++ packages/opd-model/package.js
@@ -24,14 +24,14 @@ Package.onUse(function(api) {
 
     //schemas
     'schemas/appointments-schema.js',
-    'schemas/departments-schema.js',
-    'schemas/diseases-schema.js',
-    'schemas/dispenses-schema.js',
-    'schemas/medicines-schema.js',
+    // 'schemas/departments-schema.js',
+    // 'schemas/diseases-schema.js',
+    // 'schemas/dispenses-schema.js',
+    // 'schemas/medicines-schema.js',
     'schemas/patients-schema.js',
-    'schemas/treatments-schema.js',
-    'schemas/users-schema.js',
-    'schemas/works-schema.js'
+    // 'schemas/treatments-schema.js',
+    // 'schemas/users-schema.js',
+    // 'schemas/works-schema.js'
   ]);
 
   api.export('Model');
