41a56af0ed5fd3598e8b23cabb6450b1b02666a0
benps <bentops@gmail.com>
2015-11-21 18:47:51 +0700

    add templates/medical/record-healthData.js

--- packages/opd-model/schemas/works-schema.js
+++ packages/opd-model/schemas/works-schema.js
@@ -1,19 +1,19 @@
 Schema.Works = new SimpleSchema({
-    UserID: {
-      type: String,
-      label: 'รหัสประจำบุคลากร',
-      max: 15
-    },
-    Time: {
-      type: String,
-      label: "ช่วงเวลา",
-      allowedValues: ['เช้า', 'บ่าย']
-    },
-    Day: {
-      type: String,
-      label: "วัน",
-      allowedValues: ['Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-    }, 
+  UserID: {
+    type: String,
+    label: 'รหัสประจำบุคลากร',
+    max: 15
+  },
+  Time: {
+    type: String,
+    label: "ช่วงเวลา",
+    allowedValues: ['เช้า', 'บ่าย']
+  },
+  Day: {
+    type: String,
+    label: "วัน",
+    allowedValues: ['Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+  }, 
 });
 
 //Model.Patients.attachSchema(Schema.Patients);
