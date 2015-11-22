80105093fe70cb52eb8c0dad2f1729a9b7b19ca5
benps <bentops@gmail.com>
2015-11-22 14:25:50 +0700

    disable previous date on pickadate

--- packages/opd-model/schemas/appointments-schema.js
+++ packages/opd-model/schemas/appointments-schema.js
@@ -26,14 +26,16 @@ Schema.Appointments = new SimpleSchema({
     max: 50,
     autoform: {
       type: 'hidden'
-    },
-    optional: true //false later
+    }
   },
   AppDate: {
     type: Date,
     label: 'วันที่ต้องการนัดหมาย',
     autoform: {
-      type: "pickadate"
+      type: "pickadate",
+      pickadateOptions: {
+        min: true
+      }
     }
   },
   AppTime: {
