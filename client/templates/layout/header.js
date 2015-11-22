d5e6f838fc2f7fd23179330607cb666b95668ae3
Parimeth Wongsatayanon <jame2003mvp@hotmail.com>
2015-11-22 21:16:04 +0700

    Login Layout

--- client/templates/layout/header.js
+++ client/templates/layout/header.js
@@ -18,3 +18,42 @@ Template.DashboardHeader.helpers({
 
   },
 });*/
+
+class NavHeader extends BlazeComponent {
+	onCreated() {
+    super.onCreated();
+  }
+}
+
+
+Template.NavHeader.helpers({
+/*
+	if(){
+    menu: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+    }
+    else if(){
+    	
+    }
+*/
+  });
+
+
+
+
+//tasks: [
+ /*     { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
+
+*/
+
+
+
+
+  NavHeader.register('NavHeader');
--- client/templates/layout/main-layout.js
+++ client/templates/layout/main-layout.js
@@ -7,3 +7,4 @@ class MainLayout extends BlazeComponent{
 }
 
 MainLayout.register('MainLayout');
+
