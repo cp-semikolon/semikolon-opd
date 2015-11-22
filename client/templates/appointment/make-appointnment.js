44d0782427319016c73baf61cbc749868baa6305
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-11-22 21:40:22 +0700

    make-appointment is now can add doctor but still no idea if doctor is free at a time

--- client/templates/appointment/make-appointnment.js
+++ client/templates/appointment/make-appointnment.js
@@ -1,6 +1,35 @@
 class MakeAppointment extends BlazeComponent {
+  onCreated() {
+    super.onCreated();
+    Session.setDefault('selectedDepartment', '');
+  }
   patientId() {
     return FlowRouter.getParam('patientId');
+  }
+
+  doctors() {
+    let doctorList = Meteor.users.find(
+      {
+        'profile.roles.0': 'doctor',
+        'profile.Department': Session.get('selectedDepartment')
+      }
+    ).fetch();
+
+    return doctorList.map(d => {
+      return {
+          label: `แพทย์ ${d.profile.FName} ${d.profile.LName}`,
+          value: d._id
+        };
+      }
+    );
+  }
+
+  events() {
+    return super.events().concat({
+      'change select[name="DepartmentID"]'(e) {
+        Session.set('selectedDepartment', e.target.value);
+      }
+    });
   }
 }
 
