531568cae84d9724306c062cdd5007ed7174933c
punyapats <evilpun1402@gmail.com>
2015-11-23 14:05:57 +0700

    not search empty input doctor still bug

--- client/templates/find/find-patient.js
+++ client/templates/find/find-patient.js
@@ -3,7 +3,28 @@ let Patients = OPD.Model.Patients;
 PatientsIndex = new EasySearch.Index({
   collection: Patients,
   fields: ['FName', 'LName','SSID','HN'],
-  engine: new EasySearch.Minimongo()
+  engine: new EasySearch.Minimongo({
+  	selector: function (searchObject, options, aggregation) {
+  		console.log(searchObject);
+  		if(searchObject['FName']===''){
+  			return {_id: 'non_exist'};
+  		}
+  		if(searchObject['LName']===''){
+  			return {_id: 'non_exist'};
+  		}
+  		if(searchObject['SSID']===''){
+  			return {_id: 'non_exist'};
+  		}
+  		if(searchObject['HN']===''){
+  			return {_id: 'non_exist'};
+  		}
+  		searchObject['profile.roles.0'] = 'doctor';
+  		let selector = this.defaultConfiguration().selector(searchObject, options, aggregation);	 
+  		 console.log(searchObject);
+  	return selector;
+  	}
+
+  })
 });
 
 if (Meteor.isClient) {
