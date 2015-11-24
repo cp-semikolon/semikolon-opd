2d4f030e69dc525cd577537c7e636752b22051d7
benps <bentops@gmail.com>
2015-11-24 16:28:30 +0700

    now can cancelAppointment

--- packages/opd-model/models/appointments.js
+++ packages/opd-model/models/appointments.js
@@ -1,3 +1,10 @@
 /* globals Model */ 
 
 Model.Appointments = new Meteor.Collection('appointments');
+
+Meteor.methods({
+  'Appointments.delete': function (AppId) {
+    var App = Appointments.findOne(AppId);
+    Appointments.remove(AppId);
+  }
+});
