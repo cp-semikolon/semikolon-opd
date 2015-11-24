5ca7ea3372e3a418391dbe19ee0739ffa6d3cd2a
punyapats <evilpun1402@gmail.com>
2015-11-24 16:27:55 +0700

    mod schema

---
+++ packages/opd-model/schemas/record-schema.js
@@ -0,0 +1,29 @@
+Schema.Record = new SimpleSchema({
+    patientid:{
+      type: String,
+      max: 50
+    },
+    doctorid:{
+      type:String,
+      max:50
+    },
+    Date: {
+      type: Date,
+      label: "วันที่รักษา"
+    },
+    Time:{
+      type: String,
+      max: 50
+    },
+    Health:{
+      type: Schema.HealthData
+    },
+    Med:{
+      type:Schema.MedData
+    },
+    Dispense:{
+      type:[Schema.Dispenses]
+    }
+});
+
+Model.Record.attachSchema(Schema.Record);
