df09a6f53020e0bcc005bc2ba19f959ff6314664
Worawut <virus_online-555.com@hotmail.com>
2015-11-25 17:42:36 +0700

    edit cancelWardrounds-schema

--- packages/opd-model/schemas/cancelWardrounds-schema.js
+++ packages/opd-model/schemas/cancelWardrounds-schema.js
@@ -2,19 +2,21 @@ Schema.CancelWardrounds = new SimpleSchema({
     // Doctor 
     UserID: {
       type: String,
-      label: 'รหัสประจำบุคลากร'
+      label: 'รหัสประจำบุคลากร',
+      unique: true
     },
     // Cancellation list
-    dateTime:{
+    dateTimes: {
       type: Object,
       label:'วันที่และช่วงเวลา',
       blackbox: true
     }
-    // {
-    //   Date: date,
-    //   morning: true,
-    //   afternoon: false
-    // }
+    // dateTimes = {
+    //   let d = new Date();
+    //   d.toDateString(): {morning: true, afternoon: false},  
+    //   'Fri Nov 27 2015': {morning: true, afternoon: true},
+    //   'Sat Nov 28 2015': {morning: false, afternoon: false},
+    // } [true = cancel, false = not cancel]
 });
 // Attach this to CancelWardrounds
 Model.CancelWardrounds.attachSchema(Schema.CancelWardrounds);
