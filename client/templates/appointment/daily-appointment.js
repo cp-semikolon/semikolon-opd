612a5263fa3a458ca81305e7eb48aac379cc798a
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-11-25 11:52:32 +0700

    set default view daily appointment to today's

---
+++ client/templates/appointment/daily-appointment.js
@@ -0,0 +1,55 @@
+class ViewDailyAppointment extends BlazeComponent {
+  onCreated() {
+    super.onCreated();
+    this.state = new ReactiveDict();
+
+    this.state.set('appointmentDate', new Date());
+  }
+
+  onRendered() {
+    super.onRendered();
+    // document ready
+    let date = new Date();
+    let today = [date.getFullYear(), date.getMonth(), date.getDate()];
+    $('#appointment-date').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 25, // Creates a dropdown of 15 years to control year
+      onStart() {
+          this.set( 'select', today );
+      }
+    });
+
+    this.state.set('appointmentDate', new Date(...today));
+  }
+
+  currentDateAppointments() {
+    let appointmentDate = this.state.get('appointmentDate');
+    return OPD.Model.Appointments.find({AppDate: appointmentDate})
+      .map(appointment => {
+
+        let department =
+          OPD.Model.Departments.findOne(appointment.DepartmentID);
+        let doctor = Meteor.users.findOne(appointment.DoctorID);
+        let patient = OPD.Model.Patients.findOne(appointment.PatientID);
+
+        appointment.DoctorName = 
+          `${doctor.profile.FName} ${doctor.profile.LName}`;
+
+        appointment.DepartmentName = department.Name;
+
+        appointment.PatientName = `${patient.FName} ${patient.LName}`;
+
+        return appointment;
+    });
+  }
+
+  events() {
+    return super.events().concat({
+      'change input#appointment-date'(e) {
+        this.state.set('appointmentDate', new Date(e.target.value));
+      }
+    });
+  }
+}
+
+ViewDailyAppointment.register('ViewDailyAppointment');
